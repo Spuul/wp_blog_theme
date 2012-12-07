658f0b9b2c3806c5ec379ffa4e2249a50226d43f
Marvin Conson <marvin@spuul.com>
2012-12-07 16:54:52 +0800

    Revamp Sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * The Sidebar containing the main widget area.
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
- */
+
 
 $options = twentyeleven_get_theme_options();
 $current_layout = $options['theme_layout'];
@@ -13,24 +7,26 @@ $current_layout = $options['theme_layout'];
 if ( 'content' != $current_layout ) :
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
-			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+			<div class="padding">
+				<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-				<aside id="archives" class="widget">
-					<h3 class="widget-title"><?php _e( 'Archives', 'twentyeleven' ); ?></h3>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
+					<aside id="archives" class="widget">
+						<h3 class="widget-title"><?php _e( 'Archives', 'twentyeleven' ); ?></h3>
+						<ul>
+							<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+						</ul>
+					</aside>
 
-				<aside id="meta" class="widget">
-					<h3 class="widget-title"><?php _e( 'Meta', 'twentyeleven' ); ?></h3>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside>
+					<aside id="meta" class="widget">
+						<h3 class="widget-title"><?php _e( 'Meta', 'twentyeleven' ); ?></h3>
+						<ul>
+							<?php wp_register(); ?>
+							<li><?php wp_loginout(); ?></li>
+							<?php wp_meta(); ?>
+						</ul>
+					</aside>
 
-			<?php endif; // end sidebar widget area ?>
+				<?php endif; // end sidebar widget area ?>
+			</div>	
 		</div><!-- #secondary .widget-area -->
 <?php endif; ?>
