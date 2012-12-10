2768c44dde9608b6843fc48eeb429dff91a7d2eb
Marvin Conson <marvin@spuul.com>
2012-12-10 14:50:22 +0800

    add theme switch
    Add theme switch function

--- functions.php
+++ functions.php
@@ -17,5 +17,16 @@ if ( ! function_exists( 'twentyeleven_posted_on' ) ) {
 
 }
 
+// disable theme switching
+add_action( 'admin_init', 'wplg_lock_theme' );  
+function wplg_lock_theme() {
+    global $submenu, $userdata;
+    get_currentuserinfo();
+    if ( $userdata->ID != 1 ) {
+        unset( $submenu['themes.php'][5] );
+        unset( $submenu['themes.php'][15] );
+    }
+}
+
 
 
