c00b0ab1821d7a964eedcfce9b7c575aab08ab54
Marvin Conson <marvin@spuul.com>
2012-12-26 16:43:21 +0800

    Restyling blog

--- single.php
+++ single.php
@@ -4,21 +4,21 @@ get_header(); ?>
 
 		<div id="primary">
 			<div id="content" role="main">
+				<div class="content_padding">
+					<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+						<nav id="nav-single">
+							<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
+							<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav"></span> Previous', 'twentyeleven' ) ); ?></span>
+							<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav"></span>', 'twentyeleven' ) ); ?></span>
+						</nav><!-- #nav-single -->
 
-					<nav id="nav-single">
-						<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-						<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav"></span> Previous', 'twentyeleven' ) ); ?></span>
-						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav"></span>', 'twentyeleven' ) ); ?></span>
-					</nav><!-- #nav-single -->
+						<?php get_template_part( 'content', 'single' ); ?>
 
-					<?php get_template_part( 'content', 'single' ); ?>
+						<?php comments_template( '', true ); ?>
 
-					<?php comments_template( '', true ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
+					<?php endwhile; // end of the loop. ?>
+				</div><!-- content padding -->	
 			</div><!-- #content -->
 		</div><!-- #primary -->
 <?php get_sidebar(); ?>
