c776ad8203e90021dd2dcff038b544c30beb6fd7
Marvin Conson <marvin@spuul.com>
2012-12-27 18:24:42 +0800

    Fix Ribbon alignment

--- js/manual.js
+++ js/manual.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 	/* Ribbon Animation */
 	var ribbon = $('.ribbon a');
 	$(ribbon).hover(function(){
-		$(this).animate({width: '+=50'},250);
+		$(this).animate({width: '+=11'},250);
 	},function(){
-		$(this).animate({width: '-=50'},250);
+		$(this).animate({width: '-=10'},250);
 	});
+});
 
+$(document).ready(function(){
 	/* Spuul Logo animation */
 	$('.spuul_logo').hover(function(){
 		$(this).animate({top: '-=5'},200);
